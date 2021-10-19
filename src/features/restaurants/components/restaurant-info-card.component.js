a85b41f5983bd4acf89e6f657e2bdf4cd9898c3e
hsimao <e087754958@gmail.com>
2021-10-19 13:55:18 +0800

    feat: info cart

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text, View, Platform } from 'react-native';
+import { Text, View, Image, Platform } from 'react-native';
 import styled from 'styled-components/native';
 import { Card } from 'react-native-paper';
 import Star from '../../../../assets/star.svg';
+import Open from '../../../../assets/open.svg';
 
 const isWeb = Platform.OS === 'web';
 
@@ -30,39 +31,80 @@ const Info = styled(View)`
   padding: ${props => props.theme.space[3]};
 `;
 
+const Section = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
 const Rating = styled(View)`
   flex-direction: row;
   padding-top: ${props => props.theme.space[2]};
   padding-bottom: ${props => props.theme.space[2]};
 `;
 
+const CloseText = styled(Text)`
+  color: ${props => props.theme.colors.text.error};
+  margin-right: ${props => props.theme.space[2]};
+`;
+
+const IconImage = styled(Image)`
+  width: 15px;
+  height: 15px;
+  margin-left: ${props => props.theme.space[2]};
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = 'Some Restaurant',
-    icon,
+    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos = ['https://picsum.photos/500/300/?random'],
     address = '100 some random street',
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
+
+  const StarIconList = ratingArray.map((item, index) =>
+    isWeb ? (
+      <img key={index} src={Star} width={20} height={20} />
+    ) : (
+      <Star key={index} width={20} height={20} />
+    )
+  );
+
+  const OpenIcon = isWeb ? (
+    <img src={Open} width={20} height={20} />
+  ) : (
+    <Open width={20} height={20} />
+  );
 
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
-        <Rating>
-          {ratingArray.map((item, index) =>
-            isWeb ? (
-              <img key={index} src={Star} width={20} height={20} />
-            ) : (
-              <Star key={index} width={20} height={20} />
-            )
-          )}
-        </Rating>
+
+        <Section>
+          <Rating>{StarIconList}</Rating>
+
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <CloseText variant="label">CLOSED TEMPORARILY</CloseText>
+            )}
+            {OpenIcon}
+            <IconImage source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
+
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
