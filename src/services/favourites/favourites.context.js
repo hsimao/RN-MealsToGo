7e48e8b62fd9a4864a818d9a5f376b27e746112b
hsimao <e087754958@gmail.com>
2021-10-23 22:32:55 +0800

    feat: add favourites services context

---
+++ src/services/favourites/favourites.context.js
@@ -0,0 +1,30 @@
+import React, { createContext, useState } from 'react';
+
+export const FavouritesContext = createContext();
+
+export const FavouritesContextProvider = ({ children }) => {
+  const [favourites, setFavourites] = useState({});
+
+  const addFavourites = restaurant => {
+    setFavourites([...favourites, restaurant]);
+  };
+
+  const removeFavourite = restaurant => {
+    const withoutRemoveFavourites = restaurant.filter(
+      item => item.placeId !== restaurant.placeId
+    );
+    setFavourites(withoutRemoveFavourites);
+  };
+
+  return (
+    <FavouritesContext.Provider
+      value={{
+        favourites,
+        addFavourites,
+        removeFavourite,
+      }}
+    >
+      {children}
+    </FavouritesContext.Provider>
+  );
+};
