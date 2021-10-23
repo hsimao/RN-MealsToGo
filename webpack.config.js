5ba6844e85082c72af332b57cb2dbc69e21ad557
hsimao <e087754958@gmail.com>
2021-10-23 18:13:48 +0800

    feat: map search  地圖連動

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config');
-
-// Expo CLI will await this method so you can optionally return a promise.
-module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
-
-  // Finally return the new config for the CLI to use.
-  return config;
-};
