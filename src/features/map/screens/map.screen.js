1ff4b2bbe9c0749b55a22730b68ad6f2e88b69df
hsimao <e087754958@gmail.com>
2021-10-23 17:53:38 +0800

    feat: map search bar

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import MapView from 'react-native-maps';
 import styled from 'styled-components';
+import { Search } from '../components/search.component';
 
 const Map = styled(MapView)`
   height: 100%;
@@ -9,6 +10,7 @@ const Map = styled(MapView)`
 
 export const MapScreen = () => (
   <>
+    <Search />
     <Map />
   </>
 );
