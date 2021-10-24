d29a391f25ffa5c88d92ed148b57571333dbfb6d
hsimao <e087754958@gmail.com>
2021-10-25 00:05:03 +0800

    feat: firebase auth login

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components/native';
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
 import { LocationContextProvider } from './src/services/location/location.context';
 import { FavouritesContextProvider } from './src/services/favourites/favourites.context';
 import './src/config/firebase';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 import {
   useFonts as useOswald,
@@ -16,6 +17,25 @@ import { theme } from './src/infrastructure/theme';
 import { Navigation } from './src/infrastructure/navigation';
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const auth = getAuth();
+    console.log('auth', auth);
+    signInWithEmailAndPassword(auth, 'mars@gmail.com', '123456')
+      .then(userCredential => {
+        // Signed in
+        const user = userCredential.user;
+        if (user) {
+          setIsAuthenticated(true);
+        }
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        console.log('error', errorMessage);
+      });
+  }, []);
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -27,6 +47,8 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
+  if (!isAuthenticated) return null;
 
   return (
     <>
