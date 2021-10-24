f357c65746d11f53768302e84460ec67cc85b144
hsimao <e087754958@gmail.com>
2021-10-24 18:07:09 +0800

    feat: favourites data storge to locale device

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,9 +1,31 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
+
+  const saveFavourites = async value => {
+    if (value && value.length) {
+      try {
+        await AsyncStorage.setItem('@favourites', JSON.stringify(value));
+      } catch (e) {
+        console.log('saveFavourites error: ', e);
+      }
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@favourites');
+      if (value !== null) {
+        setFavourites(JSON.parse(value));
+      }
+    } catch (e) {
+      console.log('loadFavourites error: ', e);
+    }
+  };
 
   const addFavourites = restaurant => {
     setFavourites([...favourites, restaurant]);
@@ -15,6 +37,15 @@ export const FavouritesContextProvider = ({ children }) => {
     );
     setFavourites(withoutRemoveFavourites);
   };
+
+  // 第一次掛載時先從本地裝置取得資料
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
 
   return (
     <FavouritesContext.Provider
