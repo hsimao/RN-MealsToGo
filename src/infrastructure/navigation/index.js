1e319929ceb080f8ac73beec9b0a2e675598e9f7
hsimao <e087754958@gmail.com>
2021-10-26 22:37:09 +0800

    feat: basic account naviagtor and screen

--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { AppNavigator } from './app.navigator';
+import { AccountNavigator } from './account.navigator';
+import { AuthContext } from '../../services/auth/auth.context';
 
 export const Navigation = () => {
-  return <AppNavigator />;
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return (
+    <NavigationContainer>
+      {isAuthenticated ? <AppNavigator /> : <AccountNavigator />}
+    </NavigationContainer>
+  );
 };
