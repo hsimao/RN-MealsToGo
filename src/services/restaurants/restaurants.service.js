2335c655dee9260f095d9fd3da5ef273b7c7eac9
hsimao <e087754958@gmail.com>
2021-11-07 17:32:59 +0800

    feat: restaurants mock data use firebase functions

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,22 +1,42 @@
 import { mocks, mockImages } from './mock';
 import camelize from 'camelize';
+import { Platform } from 'react-native';
 
 export const fetchRestaurants = location => {
+  const isWeb = Platform.OS === 'web';
+  return isWeb
+    ? fetchRestaurantsFromLocal(location)
+    : fetchRestaurantsFromFirebase(location);
+};
+
+const fetchRestaurantsFromFirebase = location => {
+  return fetch(
+    `http://localhost:5001/mealstogo-329909/us-central1/placesNearby?location=${location}`
+  ).then(res => res.json());
+};
+
+export const fetchRestaurantsFromLocal = location => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
     if (!mock) {
       reject('not found');
     }
+
+    const addMockImage = restaurant => {
+      const rendomImage =
+        mockImages[Math.ceil(Math.random() * mockImages.length)];
+      restaurant.photos = [rendomImage];
+      return restaurant;
+    };
+
+    mock.results = mock.results.map(addMockImage);
+
     resolve(mock);
   });
 };
 
 export const restaurantsTransform = ({ results = [] }) => {
   const formatResults = results.map(item => {
-    item.photos = item.photos.map(p => {
-      return mockImages[Math.ceil(Math.random() * mockImages.length)];
-    });
-
     return {
       ...item,
       address: item.vicinity,
