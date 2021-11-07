140e9ec28178b227c51da28fc30d83325f1e6848
hsimao <e087754958@gmail.com>
2021-11-07 14:59:30 +0800

    feat: 儲存照片 logic

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,12 +1,18 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import { View } from 'react-native';
 import { Button } from 'react-native-paper';
+import { MaterialIcons } from '@expo/vector-icons';
 import { Text } from '../../../components/text.component';
+import { Spacer } from '../../../components/spacer.component';
 import { colors } from '../../../infrastructure/theme/colors';
 import styled from 'styled-components/native';
 import { Camera } from 'expo-camera';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AuthContext } from '../../../services/auth/auth.context';
 
-const ProfileCamera = styled(Camera)`
+const ProfileCamera = styled(Camera).attrs({
+  ratio: '16:9',
+})`
   width: 100%;
   height: 100%;
 `;
@@ -25,11 +31,11 @@ export const CameraButton = styled(Button).attrs({
   padding: ${props => props.theme.space[2]};
 `;
 
-export const CameraScreen = () => {
+export const CameraScreen = ({ navigation }) => {
   const cameraRef = useRef();
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.front);
+  const { user } = useContext(AuthContext);
 
+  const [type, setType] = useState(Camera.Constants.Type.front);
   const toggleType = () => {
     setType(
       type === Camera.Constants.Type.back
@@ -38,6 +44,7 @@ export const CameraScreen = () => {
     );
   };
 
+  const [hasPermission, setHasPermission] = useState(null);
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
@@ -45,9 +52,18 @@ export const CameraScreen = () => {
     })();
   }, []);
 
+  const handleTakePicture = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
+      navigation.goBack();
+    }
+  };
+
   if (hasPermission === null) {
     return <View />;
   }
+
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
@@ -56,11 +72,21 @@ export const CameraScreen = () => {
     <ProfileCamera ref={camera => (cameraRef.current = camera)} type={type}>
       <CameraButtonContainer>
         <CameraButton
-          icon="lock-open-outline"
+          icon={() => (
+            <MaterialIcons name="flip-camera-android" size={16} color="white" />
+          )}
           mode="contained"
-          onPress={() => toggleType()}
+          onPress={toggleType}
         >
           Flip
+        </CameraButton>
+        <Spacer position="left" size="large" />
+        <CameraButton
+          icon="camera-outline"
+          mode="contained"
+          onPress={handleTakePicture}
+        >
+          TakePicture
         </CameraButton>
       </CameraButtonContainer>
     </ProfileCamera>
--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,11 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useCallback, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
+import { useFocusEffect } from '@react-navigation/native';
 import { List, Avatar } from 'react-native-paper';
 import { Text } from '../../../components/text.component';
 import { Spacer } from '../../../components/spacer.component';
 import { SafeArea } from '../../../components/safe-area.component';
 import { AuthContext } from '../../../services/auth/auth.context';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SettingsItem = styled(List.Item)`
   padding: ${props => props.theme.space[3]};
@@ -17,12 +19,34 @@ const AvatarContainer = styled.View`
 
 export const SettingsScreen = ({ navigation }) => {
   const { onLogout, user } = useContext(AuthContext);
+  const [photo, setPhoto] = useState(null);
+
+  const getProfilePicture = async currentUser => {
+    const photoUri = await AsyncStorage.getItem(`${currentUser.uid}-photo`);
+    setPhoto(photoUri);
+  };
+
+  // NOTE: 使用 useFocusEffect, 可在每次導航改變時觸發
+  useFocusEffect(
+    useCallback(() => {
+      getProfilePicture(user);
+    }, [user])
+  );
 
   return (
     <SafeArea>
       <AvatarContainer>
         <TouchableOpacity onPress={() => navigation.navigate('Camera')}>
-          <Avatar.Icon size={100} icon="human" backgroundColor="#2182BD" />
+          {!photo && (
+            <Avatar.Icon size={100} icon="human" backgroundColor="#2182BD" />
+          )}
+          {photo && (
+            <Avatar.Image
+              size={100}
+              source={{ uri: photo }}
+              backgroundColor="#2182BD"
+            />
+          )}
         </TouchableOpacity>
         <Spacer position="top" size="large">
           <Text variant="label">{user.email}</Text>
