743760b1728ea392a54d22259c7640a98bb9dd74
hsimao <e087754958@gmail.com>
2021-11-13 17:41:16 +0800

    feat: 串接 real google map api

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,10 +1,28 @@
+const functions = require('firebase-functions');
 const { location: locationMock } = require('./geocode.mock');
 const url = require('url');
 
-module.exports.geocodeRequest = (request, response) => {
-  const { city } = url.parse(request.url, true).query;
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock } = url.parse(request.url, true).query;
 
-  const location = city ? locationMock[city.toLowerCase()] : '';
+  if (mock === 'true') {
+    const location = city ? locationMock[city.toLowerCase()] : '';
+    return response.json(location);
+  }
 
-  response.json(location);
+  client
+    .geocode({
+      params: {
+        address: city,
+        key: functions.config().google.mapkey,
+      },
+      timeout: 1000,
+    })
+    .then(res => {
+      return response.json(res.data);
+    })
+    .catch(err => {
+      response.status(400);
+      return response.send(err.response.data.error_message);
+    });
 };
--- functions/places/index.js
+++ functions/places/index.js
@@ -1,13 +1,35 @@
+const functions = require('firebase-functions');
 const { mocks, addMockImage } = require('./mock');
 const url = require('url');
 
-module.exports.placesRequest = (request, response) => {
-  const { location } = url.parse(request.url, true).query;
+module.exports.placesRequest = (request, response, client) => {
+  const { location, mock } = url.parse(request.url, true).query;
 
-  const data = location ? mocks[location] : '';
-  if (data) {
-    data.results = data.results.map(addMockImage);
+  if (mock === 'true') {
+    const data = location ? mocks[location] : '';
+    if (data) {
+      data.results = data.results.map(addMockImage);
+    }
+    return response.send(data);
   }
 
-  response.send(data);
+  client
+    .placesNearby({
+      params: {
+        location,
+        radius: 1500, // 半徑
+        type: 'restaurant',
+        key: functions.config().google.mapkey,
+      },
+      timeout: 1000,
+    })
+    .then(res => {
+      // NOTE: google map 圖片要另外處理, 這邊用假圖
+      res.data.results = res.data.results.map(addMockImage);
+      return response.json(res.data);
+    })
+    .catch(err => {
+      response.status(400);
+      return response.send(err.response.data.error_message);
+    });
 };
